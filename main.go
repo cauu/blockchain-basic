13849d847b8ff02e6f278e4b68cb373fc28f4240
Martin <cauu@163.com>
2018-01-08 01:00:42 +0800

    init

---
+++ main.go
@@ -0,0 +1,11 @@
+type Block {
+	Timestamp int64
+	Data []byte
+	PrevBlockHash []byte
+	Hash []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+}
